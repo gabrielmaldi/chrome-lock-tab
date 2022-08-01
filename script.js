b71f4743afc4fba148c0e13365faef6b4628728e
Gabriel Monteagudo <gabrielmaldi@hotmail.com>
2022-08-01 14:10:17 -0300

    Prepend 🔒 to a locked tab's title

--- script.js
+++ script.js
@@ -1,17 +1,22 @@
 (function () {
+  const titlePrefix = "🔒 ";
+
   let isLocked = false;
+  let titleObserver = null;
 
   chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
     switch (request.message) {
       case "setIsLocked":
         isLocked = request.value;
+        setTitle();
+
         break;
     }
 
     sendResponse();
   });
 
-  window.addEventListener("beforeunload", function (event) {
+  window.addEventListener("beforeunload", (event) => {
     if (!isLocked) {
       return;
     }
@@ -23,4 +28,37 @@
     event.returnValue = "[LockTab] Are you sure you want to exit?";
     return event.returnValue;
   }, true);
+
+  function setTitle() {
+    if (isLocked) {
+      if (!document.title.startsWith(titlePrefix)) {
+        document.title = titlePrefix + document.title;
+      }
+
+      if (!titleObserver) {
+        let titleElement = document.querySelector("head > title");
+        if (titleElement) {
+          titleObserver = new MutationObserver((mutations) => {
+            if (!document.title.startsWith(titlePrefix)) {
+              document.title = titlePrefix + document.title;
+            }
+          });
+
+          titleObserver.observe(titleElement, {
+            subtree: true,
+            childList: true,
+          });
+        }
+      }
+    } else {
+      if (document.title.startsWith(titlePrefix)) {
+        document.title = document.title.substring(titlePrefix.length);
+      }
+
+      if (titleObserver) {
+        titleObserver.disconnect();
+        titleObserver = null;
+      }
+    }
+  }
 })();
